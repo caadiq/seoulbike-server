6a8e24a6aebea03072e62abcdd0a7f50273e973c
반명우 <caadiq@gmail.com>
2025-01-08 13:14:27 +0900

    @EnableJpaAuditing 추가

--- src/main/kotlin/com/beemer/seoulbike/SeoulbikeApplication.kt
+++ src/main/kotlin/com/beemer/seoulbike/SeoulbikeApplication.kt
@@ -3,10 +3,12 @@ package com.beemer.seoulbike
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableCaching
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 class SeoulbikeApplication
 
